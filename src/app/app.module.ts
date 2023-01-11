5b745292ce56ceb1e67fa18c5187a5d0bfa433fd
Elisa Capololo <elisacapololo@meltwater.org>
2023-01-11 11:45:55 +0000

    first commit after creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainBodyComponent } from './main-body/main-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponentComponent,
+    SideBarComponent,
+    FooterComponent,
+    MainBodyComponent
   ],
   imports: [
     BrowserModule,
