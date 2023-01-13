934a63c65a29b16677609268e6199fac87df2537
Elisa Capololo <elisacapololo2017@gmail.com>
2023-01-13 05:09:51 +0000

    add routing in our project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectivesComponent } from './directives/directives.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { ParentComponent } from './parent/parent.component';
+import { TaskNumberComponent } from './task-number/task-number.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'new-component', pathMatch: 'full'},
+  {path: 'new-component', component: NewComponentComponent},
+  {path: 'main-body', component: MainBodyComponent},
+  {path: 'directives', component: DirectivesComponent},
+  {path: 'task-number', component: TaskNumberComponent},
+  {path: 'parent', component: ParentComponent},
+  {path: '***', redirectTo: 'new-component'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
